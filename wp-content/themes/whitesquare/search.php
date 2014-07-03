99716f813376e32915540a8d8e01fb8a808e5173
Mirantus <the-ms@yandex.ru>
2014-07-03 14:14:41 +0700

    fix search text

--- wp-content/themes/whitesquare/search.php
+++ wp-content/themes/whitesquare/search.php
@@ -4,7 +4,7 @@
 </div>
 <?php get_sidebar(); ?>
 <section>
-	<h2 class="content-heading"><?php printf( __( 'Search Results for: %s', 'twentyfourteen' ), get_search_query() ); ?></h2>
+	<h2 class="content-heading"><?php printf( __( 'Search Results for: %s', 'default' ), get_search_query() ); ?></h2>
 	<?php if (have_posts()): while (have_posts()): the_post(); ?>
 		<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 		<p><?php the_excerpt(); ?></p>
